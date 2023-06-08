be788c4ce7a35cbcfac75a324ab0f67ab3ad99d5
Alejandro <aavila0707@gmail.com>
2023-06-08 15:18:10 +0200

    feat: update augment dataset

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from augment import augment_df
 
+
 def get_json_data():
     df = pd.read_json("data/data.txt")
     df = df.drop("found_duplicate", axis=1)
@@ -9,8 +10,12 @@ def get_json_data():
 
 def main():
     df = get_json_data()
-
     
+    print("Original dataframe:", df.head())
+    
+    augmented_df = augment_df(df.head())
+    
+    print("Augmented dataframe:", augmented_df.head(10))
 
 
 if __name__ == "__main__":
